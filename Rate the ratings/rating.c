2d6e6ec1d85387ca3a0add14bb45440042a06b02
Amancom <72262815+Amancom@users.noreply.github.com>
2020-10-04 21:43:08 +0530

    Raters can become successful person.

---
+++ Rate the ratings/rating.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int main()
+{
+    int rating;
+    printf("Enetr your rating rating (1-5) \n");
+    scanf("%d", &rating);
+    switch (rating)
+    {
+    case 1:
+    printf("Your rating is 1");
+    break;
+        case 2:
+    printf("Your rating is 2");
+     break;
+    case 3:
+    printf("Your rating is 3");
+     break;
+    case 4:
+    printf("Your rating is 4");
+     break;
+    case 5:
+    printf("Your rating is 5");
+    break;
+    default:
+    printf("Invalid rating");
+    
+    
+        
+    }
+
+    return 0;
+}
